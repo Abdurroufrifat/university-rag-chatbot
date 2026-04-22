3776844e938e9ab351dcb8086eb99fd3658572e9
Abdur Rouf <89192811+Abdurroufrifat@users.noreply.github.com>
2026-04-22 14:23:43 +0800

    Add files via upload

--- src/retrieval/retriever.py
+++ src/retrieval/retriever.py
@@ -1,6 +1,6 @@
 from src.embeddings.embedder import embed_query
 
-def retrieve_documents(query, store, k=3):
+def retrieve_documents(query, store, k=8):
     query_embedding = embed_query(query)
     results = store.search(query_embedding, k=k)
     return results
